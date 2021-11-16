f9d107a70306e7c037822277aae7fd912b4ef8f4
Pittawat <pete.pittawas@gmail.com>
2021-11-16 21:46:11 +0700

    update: get detail user in setting page

--- app/src/main/java/com/example/myshop/activities/activity/SettingActivity.kt
+++ app/src/main/java/com/example/myshop/activities/activity/SettingActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myshop.R
 import com.example.myshop.databinding.ActivitySettingBinding
 import com.example.myshop.firebase.FireStoreClass
@@ -11,6 +12,7 @@ import com.example.myshop.model.User
 import com.example.myshop.util.BaseCommon
 import com.google.firebase.auth.FirebaseAuth
 import com.example.myshop.activities.activity.UserProfileActivity.Companion.ACTION_EDIT_INFO
+import com.example.myshop.util.GlideLoader
 
 class SettingActivity : BaseActivity(),BaseCommon {
     private var currentUser : User? = null
@@ -27,13 +29,14 @@ class SettingActivity : BaseActivity(),BaseCommon {
         setListener()
     }
 
-    override fun setToolbar() {
-
+    override fun onResume() {
+        super.onResume()
+        getDetailOfUser()
     }
 
-    override fun setUI() {
-        FireStoreClass().getCurrentUserByID(this)
-    }
+    override fun setToolbar() {}
+
+    override fun setUI() {}
 
     override fun setListener() {
         binding.back.setOnClickListener {
@@ -53,13 +56,20 @@ class SettingActivity : BaseActivity(),BaseCommon {
         finish()
     }
 
+    private fun getDetailOfUser(){
+        showProgressDialog()
+        FireStoreClass().getCurrentUserByID(this)
+    }
+
     fun getDataUser(result : User?){
+        Log.i("result",result.toString())
         currentUser = result
         result?.let {
             binding.name.text = "${it.firstname} ${it.lastname}"
             binding.gender.text = it.gender
             binding.email.text = it.email
             binding.phoneNumber.text = it.mobile.toString()
+            GlideLoader(this).loadImage(it.image,binding.image)
         }
     }
 
