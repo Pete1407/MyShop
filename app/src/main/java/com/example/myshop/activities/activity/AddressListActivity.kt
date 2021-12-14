6cd8ff227d91d0235e399336c3f85facf8a76737
Pittawat <pete.pittawas@gmail.com>
2021-12-14 17:35:45 +0700

    update code

---
+++ app/src/main/java/com/example/myshop/activities/activity/AddressListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myshop.activities.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myshop.R
+
+class AddressListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address_list)
+    }
+}
