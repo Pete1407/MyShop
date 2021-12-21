5a4869eae1faf615172e07d5ca9c23c725b566a7
Pittawat <pete.pittawas@gmail.com>
2021-12-21 16:58:30 +0700

    update code

--- app/src/main/java/com/example/myshop/activities/activity/AddAddressActivity.kt
+++ app/src/main/java/com/example/myshop/activities/activity/AddAddressActivity.kt
@@ -2,14 +2,18 @@ package com.example.myshop.activities.activity
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.myshop.R
 import com.example.myshop.databinding.ActivityAddAddressBinding
+import com.example.myshop.model.AddressModel
 import com.example.myshop.util.BaseCommon
 
-class AddAddressActivity : BaseActivity(), BaseCommon {
+class AddAddressActivity : BaseActivity(), BaseCommon{
 
+    private var address : AddressModel? = null
+    private var place : String = ""
     private val binding: ActivityAddAddressBinding by lazy {
         ActivityAddAddressBinding.inflate(layoutInflater)
     }
@@ -17,6 +21,9 @@ class AddAddressActivity : BaseActivity(), BaseCommon {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_address)
+        setToolbar()
+        setUI()
+        setListener()
     }
 
     override fun setToolbar() {
@@ -28,7 +35,35 @@ class AddAddressActivity : BaseActivity(), BaseCommon {
     }
 
     override fun setListener() {
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+        binding.submit.setOnClickListener {
+            Log.i("result","click...")
+        }
+    }
 
+    private fun detectInput():Boolean{
+        if(binding.fullname.text!!.isBlank()){
+            showSnackBar(getString(R.string.error_msg_enter_full_name),true)
+            return false
+        }else if(binding.phoneNumber.text!!.isBlank()){
+            showSnackBar(getString(R.string.error_msg_enter_phone_number),true)
+            return false
+        }else if(binding.address.text!!.isBlank()){
+            showSnackBar(getString(R.string.error_msg_enter_address),true)
+            return false
+        }else if(binding.zipcode.text!!.isBlank()){
+            showSnackBar(getString(R.string.error_msg_enter_zip_code),true)
+            return false
+        }else if(binding.note.text!!.isBlank()){
+            showSnackBar(getString(R.string.error_msg_enter_note),true)
+            return false
+        }else if(place.isBlank()){
+            showSnackBar(getString(R.string.error_msg_enter_place),true)
+            return false
+        }
+        return true
     }
 
     companion object{
