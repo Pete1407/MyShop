0b6cb64aa378dfcca0db83b8f90d287757b3c252
Pittawat <pete.pittawas@gmail.com>
2022-02-16 17:03:55 +0700

    add crashlytic

--- app/src/main/java/com/example/myshop/firebase/FireStoreClass.kt
+++ app/src/main/java/com/example/myshop/firebase/FireStoreClass.kt
@@ -1,9 +1,7 @@
 package com.example.myshop.firebase
 
 import android.app.Activity
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.myshop.activities.activity.*
 import com.example.myshop.activities.fragment.DashboardFragment
